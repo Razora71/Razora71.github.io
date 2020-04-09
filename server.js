948c1778e7dc080644575a08d60cdfa07c83adc9
Razora71 <61700819+Razora71@users.noreply.github.com>
2020-04-10 00:43:00 +0300

    фы

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-//import io from 'https://cdn.jsdelivr.net/npm/socket.io-client@2/dist/socket.io.js';
-
-
 var express = require('express'); // Express contains some boilerplate to for routing and such
 var app = express();
 var http = require('http').Server(app);
-
 var io = require('socket.io')(http);
 
-// Serve the index page 
+// Serve the index page
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
 // Listen on port 5000
-app.set('port', (process.env.PORT || 5000));
+/*app.set('port', (process.env.PORT || 5000));
 http.listen(app.get('port'), function(){
   console.log('listening on port',app.get('port'));
+});*/
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-alert('lox');
-
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
