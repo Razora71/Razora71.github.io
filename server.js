be01808150b733fbbb212cf6c6a3fb1bedef45dd
Razora71 <61700819+Razora71@users.noreply.github.com>
2020-04-09 23:51:39 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+import io from 'https://cdn.jsdelivr.net/npm/socket.io-client@2/dist/socket.io.js';
+
+
+var express = require('express'); // Express contains some boilerplate to for routing and such
+var app = express();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+// Serve the index page 
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+// Listen on port 5000
+app.set('port', (process.env.PORT || 5000));
+http.listen(app.get('port'), function(){
+  console.log('listening on port',app.get('port'));
+});
+
+alert('lox');
+
